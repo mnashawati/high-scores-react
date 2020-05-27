8fffa256de541f7a784a5dd1b59d0bcb6be982e2
Mo Nashawati <mo.nashawati@gmail.com>
2020-05-27 11:54:58 +0100

    Sorting by country names and by scores

--- src/App.js
+++ src/App.js
@@ -9,21 +9,27 @@ function App() {
     <div className="blue-container">
       <h1 className="heading">High Scores per Country</h1>
       <div>
-        {data.map((obj, index) => (
-          <div key={index} className="green-container">
-            <h2 className="heading">HIGH SCORES: {obj.name}</h2>
-            <div className="scores">
-              {obj.scores.map((score, index) => (
-                <div className="score" key={index}>
-                  <span className="score-n">{score.n.toUpperCase()}</span>
-                  <span className="score-s">{score.s.toString()}</span>
-                  {/* {"\n"} */}
-                  {/* <hr /> */}
-                </div>
-              ))}
+        {data
+          .sort((a, b) =>
+            a.name.toUpperCase() > b.name.toUpperCase() ? 1 : -1
+          )
+          .map((obj, index) => (
+            <div key={index} className="green-container">
+              <h2 className="heading">HIGH SCORES: {obj.name}</h2>
+              <div className="scores">
+                {obj.scores
+                  .sort((scoreA, scoreB) =>
+                    Number(scoreA.s) < Number(scoreB.s) ? 1 : -1
+                  )
+                  .map((score, index) => (
+                    <div className="score" key={index}>
+                      <span className="score-n">{score.n.toUpperCase()}</span>
+                      <span className="score-s">{Number(score.s)}</span>
+                    </div>
+                  ))}
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
       </div>
     </div>
   );
